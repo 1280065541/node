f5133f8a1d6a41281f6b749731110cc08b957c11
yanlu <1280065541@qq.com>
2021-04-19 00:03:57 +0800

    yl:新增eslint配置

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+        commonjs: true,
+    },
+    extends: 'eslint:recommended', //启用推荐规则
+    parserOptions: {
+        ecmaVersion: 12,
+        sourceType: 'module',
+    },
+    rules: {},
+};
