787421082bcea13b41ac8ffa0b181206e72d3af0
yanlu <1280065541@qq.com>
2021-04-19 23:43:58 +0800

    yl:配置目录，学习promise

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
         es2021: true,
         commonjs: true,
     },
-    extends: 'eslint:recommended', //启用推荐规则
+    // extends: 'eslint:recommended', //启用推荐规则
     parserOptions: {
         ecmaVersion: 12,
         sourceType: 'module',
